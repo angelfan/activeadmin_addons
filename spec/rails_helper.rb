7cdd61cf3e1d737f6fc4e52a019337cdee3bb6e2
oaestay <oaestay@uc.cl>
2017-12-22 18:07:45 -0300

    fixup! chore(Capybara): Add chromedriver --headless config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,18 +38,16 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  Capybara.register_driver(:headless_chrome) do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu] }
-    )
-
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      desired_capabilities: capabilities
-    )
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
 
+  Capybara.register_driver :headless_chrome do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless) }
+    )
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  end
   Capybara.javascript_driver = :headless_chrome
 
   config.include ActiveAdminHelpers
